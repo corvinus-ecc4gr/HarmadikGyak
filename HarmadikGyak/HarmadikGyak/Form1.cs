08433db9ff4ac8b8b9c6b3fbd080fbae1a79da9d
Dániel Szántó <daniel.szanto@stud.uni-corvinus.hu>
2021-10-14 21:46:16 +0200

    excel kiíratás

--- HarmadikGyak/HarmadikGyak/Form1.cs
+++ HarmadikGyak/HarmadikGyak/Form1.cs
@@ -14,12 +14,14 @@ namespace HarmadikGyak
 {
     public partial class Form1 : Form
     {
-        RealEstateEntities context = new RealEstateEntities();
+        private int _million = (int)Math.Pow(10, 6);
 
+        RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
         Excel.Application xlApp;
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
+        string[] headers;
 
         public Form1()
         {
@@ -27,6 +29,7 @@ namespace HarmadikGyak
             LoadData();
             dataGridView1.DataSource = lakasok;
             CreateExcel();
+            FormatTable();
 
         }
 
@@ -46,6 +49,7 @@ namespace HarmadikGyak
                 xlSheet = xlWB.ActiveSheet;
 
                 CreateTable();
+                
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -59,7 +63,6 @@ namespace HarmadikGyak
                 xlApp.Quit();
                 xlWB = null;
                 xlApp = null;
-                throw;
             }
             finally
             {
@@ -69,6 +72,91 @@ namespace HarmadikGyak
 
         private void CreateTable()
         {
+            headers = new string[]
+            
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[lakasok.Count, headers.Length];
+
+            int counter = 0;
+            int floorcolumn = 6;
+            foreach (var lakas in lakasok)
+            {
+                values[counter, 0] = lakas.Code;
+                values[counter, 1] = lakas.Vendor;
+                values[counter, 2] = lakas.Side;
+                values[counter, 3] = lakas.District;
+
+                if (lakas.Elevator)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+
+                values[counter, 5] = lakas.NumberOfRooms;
+                values[counter, 6] = lakas.FloorArea;
+                values[counter, 7] = lakas.Price;
+                values[counter, 8] = string.Format("={0}/{1}*{2}",
+                    "H" + (counter + 2).ToString(),
+                    GetCell(counter + 2, floorcolumn + 1),
+                    _million.ToString());
+
+                counter++;
+            }
+
+            var range = xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(values.GetLength(0), values.GetLength(1)));
+
+            range.Value2 = values;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormatTable()
+        {
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
 
         }
 
