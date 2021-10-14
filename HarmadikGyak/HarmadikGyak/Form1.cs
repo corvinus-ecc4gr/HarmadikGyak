a77c0d9f13ec6d9f4f6dd985cdcd326fc9c8c429
Dániel Szántó <daniel.szanto@stud.uni-corvinus.hu>
2021-10-14 17:40:03 +0200

    data load

--- HarmadikGyak/HarmadikGyak/Form1.cs
+++ HarmadikGyak/HarmadikGyak/Form1.cs
@@ -12,9 +12,26 @@ namespace HarmadikGyak
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+
+        List<Flat> lakasok;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
